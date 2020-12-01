1068fb8283950b6f1cc7eec53167808e259b6cad
RajeshNakum <rajesh.nakum.7756@gmail.com>
2020-12-01 10:49:31 +0530

    first commit

---
+++ jsondate.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // using array
+        $date =array ("date" => date('d-m-Y'));
+        echo json_encode($date);
+        echo "<br>";
+
+    // using object
+        echo json_encode($date,JSON_FORCE_OBJECT);
+
+    ?>
+</body>
+</html>
